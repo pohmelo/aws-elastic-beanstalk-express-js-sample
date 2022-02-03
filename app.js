c24779ef206fbba89f9ad1ca4e3ca197408f7679
pohmelo <mikkovp@hotmail.com>
2022-02-03 12:50:28 +0200

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('This is the text that I wanted!'));
+app.get('/', (req, res) => res.send('This is the text that I wanted even more!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
